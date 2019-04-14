90c24fb4b3b090f73f145c1b8d0ebdc603d358d6
rostirosti <rostirosti@TM4K.local>
2019-04-14 16:27:44 -0400

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const app = express();
 
 // Defines a PORT for the server to listen for requests
-const PORT = 8080;
+var PORT = process.env.PORT || 3000;
 
 // Sets up our server to parse our request body for usage
 app.use(express.urlencoded({ extended: true }));
@@ -20,6 +20,6 @@ require('./routing/apiRoutes.js')(app);
 require('./routing/htmlRoutes.js')(app);
 
 // Starts our server on the predefined PORT
-app.listen(PORT, function(){
+app.listen(PORT, function() {
   console.log(`App is now listening on PORT ${PORT}`)
 })
